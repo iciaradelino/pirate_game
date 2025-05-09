bab22411dcd0395b976d0017eeb6a56eb8b27086
Tekky <98614666+xtekky@users.noreply.github.com>
2025-05-09 14:02:48 +0200

    x

--- animation.h
+++ animation.h
@@ -3,7 +3,7 @@
 
 #include "common.h" // For GameState forward dec
 
-#define ANIMATION_FILENAME "swing_animation_frames.txt"
+#define ANIMATION_FILENAME "./ascii/swing_animation_frames.txt"
 #define FRAME_DELIMITER "---FRAME_END---"
 #define MAX_FRAME_LINES 50  // Max number of lines a frame can have
 
